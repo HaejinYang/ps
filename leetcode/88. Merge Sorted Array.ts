4e41e307e692e3593bc327557ccbd0ca6a64fee2
haejin.yang91 <haejin.yang91@gmail.com>
2022-12-30 22:12:38 +0900

    leetcode 88번 문제. 내장된 함수를 쓴 정렬(o(nlogn)을 쓰지 않고 다른 방식O(m+n)으로 해결함.

--- leetcode/88. Merge Sorted Array.ts
+++ leetcode/88. Merge Sorted Array.ts
@@ -5,3 +5,29 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     nums1.splice(m, n, ...nums2);
     nums1.sort((lhs, rhs) => lhs - rhs);
 };
+
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge2(nums1: number[], m: number, nums2: number[], n: number): void {
+    if((m + n) === 0) {
+        return;
+    }
+
+    // 인덱스 m-1과 n-1을 비교하여 큰 것을 m + n - 1로 보낸다.
+    // 보내쪽 인덱스를 -- 한다.
+    while(m > 0 && n > 0) {
+        if(nums1[m - 1] >= nums2[n - 1]) {
+            nums1[m + n - 1] = nums1[m - 1];
+            --m;
+        } else {
+            nums1[m + n - 1] = nums2[n - 1];
+            --n;
+        }
+    }
+
+    while(n > 0) {
+        nums1[n - 1] = nums2[n - 1];
+        --n;
+    }
+};
