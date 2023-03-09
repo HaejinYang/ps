ed550287287660b595d8b2886efab091bdc2a723
haejin <haejin.yang91@gmail.com>
2023-03-09 18:37:33 +0900

    필요없는 파라미터 제거

--- programmers/level2/덧칠하기.js
+++ programmers/level2/덧칠하기.js
@@ -1,5 +1,5 @@
 function solution(n, m, section) {
-    const { count } = section.reduce((acc, val, index, arr) => {
+    const { count } = section.reduce((acc, val) => {
         if(val > acc.prevPaintMax) {
             acc.prevPaintMax = val + m - 1;
             acc.count++;
