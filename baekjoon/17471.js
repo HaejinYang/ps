9edf74a1c82f4e059c3e9bc433633f4962b1689b
haejin <haejin.yang91@gmail.com>
2023-07-01 20:20:24 +0900

    https://www.acmicpc.net/problem/17471

--- baekjoon/17471.js
+++ baekjoon/17471.js
@@ -21,63 +21,18 @@ function solve() {
   testCases.forEach((row) => {
     const convertedRow = row.split(" ").map(Number);
     let masking = 0;
-    convertedRow.forEach((adj) => {
-      masking |= 1 << adj;
-    });
+    for (let i = 1; i < convertedRow.length; i++) {
+      masking |= 1 << (convertedRow[i] - 1);
+    }
 
+    masking |= 1 << rowIndex;
     adjacent[rowIndex] = masking;
     ++rowIndex;
   });
 
-  const visit = (index, result, group) => {
-    visited[index] = true;
-
-    if (result.length === group.length) {
-      const a = [...result];
-      const b = [...group];
-
-      console.log(a, b);
-      return a.sort().toString() === b.sort().toString();
-    }
-
-    const each = adjacent[index];
-    let next = 1;
-    let shift = 0;
-    while (shift < N) {
-      const comp = next << shift;
-      if (comp & each && visited[shift] === false) {
-        result.push(shift);
-        const connected = visit(shift, [...result], group);
-        if (connected) {
-          return true;
-        }
-      }
-      ++shift;
-    }
-    for (let next = 0; next < 1 << N; next++) {}
-
-    return false;
-  };
-
-  const getGroupPeople = (group) => {
-    let sum = 0;
-    group.forEach((index) => {
-      sum += people[index];
-    });
-
-    return sum;
-  };
-
-  const clearVisited = () => {
-    for (let i = 0; i < visited.length; i++) {
-      visited[i] = false;
-    }
-  };
-
   let min = Number.MAX_SAFE_INTEGER;
-  let answer = [];
-  let result1;
-  let result2;
+  let hasAnswer = false;
+  // [ 11, 55, 14, 13, 18, 34 ]
   for (let i = 0; i < 1 << N; i++) {
     let group1 = [];
     let group2 = [];
@@ -89,35 +44,66 @@ function solve() {
       }
     }
 
-    // 선거구 2개로 나누는것까진 했고
-    // 이제 선거구 안에서 서로 연결되었는지 확인해야하는데 어떻게 확인하지.
-    if (group1.length > 0 && group2.length > 0) {
-      clearVisited();
-      const group1IsConnected = visit(group1[0], [group1[0]], group1);
-      clearVisited();
-      const group2IsConnected = visit(group2[0], [group2[0]], group2);
-      //   console.log(
-      //     "group : ",
-      //     group1,
-      //     group1IsConnected,
-      //     group2,
-      //     group2IsConnected
-      //   );
-      if (group2IsConnected && group1IsConnected) {
-        const result = Math.abs(
-          getGroupPeople(group1) - getGroupPeople(group2)
-        );
+    // group1과 group2에 선거구가 담긴다.
+    if (group1.length === 0 || group2.length === 0) {
+      continue;
+    }
 
-        if (result < min) {
-          min = result;
-          result1 = [...group1];
-          result2 = [...group2];
-        }
-      }
+    clearVisited(visited);
+    let group1Result = [];
+    visit(group1, group1[0], adjacent, group2, visited, group1Result, N);
+
+    clearVisited(visited);
+    let group2Result = [];
+    visit(group2, group2[0], adjacent, group1, visited, group2Result, N);
+
+    const group1Set = new Set(group1);
+    const group1IsSame = group1Result.every(e => group1Set.has(e) && group1Set.size === group1Result.length);
+    const group2Set = new Set(group2);
+    const group2IsSame = group2Result.every((e) => group2Set.has(e) && group2Set.size === group2Result.length);
+
+    if(group1IsSame && group2IsSame) {
+      hasAnswer = true;
+      min = Math.min(Math.abs(sumPeopleInGroup(group1Result, people) - sumPeopleInGroup(group2Result, people)), min);
     }
   }
 
-  console.log(min, result1, result2);
+  if(hasAnswer) {
+    console.log(min);
+  } else {
+    console.log(-1);
+  }
+}
+
+/*
+  1. 선거구 안에 있는 구역끼리만을 이동해서 서로 연결되어 있어야 한다.
+  2. 다른 구역이 포함되면 x
+*/
+function visit(group, node, adjacent, except, visited, result, N) {
+  const masking = adjacent[node];
+
+  for (let i = 0; i < N; i++) {
+    if ((masking & (1 << i)) && !except.includes(i) && !visited[i]) {
+      visited[i] = true;
+      result.push(i);
+      visit(group, i, adjacent, except, visited, result, N);
+    }
+  }
+}
+
+function clearVisited(visited) {
+  for(let i = 0; i < visited.length; i++) {
+    visited[i] = false;
+  }
+}
+
+function sumPeopleInGroup(group, people) {
+  let sum = 0;
+  for(let i = 0; i < group.length; i++) {
+    sum += people[group[i]];
+  }
+
+  return sum;
 }
 
 solve();
